c92a09e78dfe7593882307b9ba146e39a7784d39
Ethan Parsley <ecparsle@iu.edu>
2020-10-15 18:43:33 -0400

    added correct SQL codes into this file

--- app/data/schema-patient.sql
+++ app/data/schema-patient.sql
@@ -1,27 +1,49 @@
-USE msis_triage;
+USE ocfr_v1;
+CREATE TABLE People (
+ userID INT AUTO_INCREMENT,
+ firstName varchar(255),
+ lastName varchar(255),
+ address varchar(255),
+ email varchar(255),
+ password varchar (255),
+ phoneNumber INT,
+ dob DATE,
+ gender varchar(255),
+ startDate DATE,
+ departmentPosition varchar(255),
+ radioNumber int,
+ stationNumber int,
+ isActive bool,
+ PRIMARY KEY (userID)
+ );
 
-CREATE TABLE Patient (
-    patientGuid VARCHAR(64) PRIMARY KEY,
-    firstName VARCHAR(64),
-    lastName VARCHAR(64),
-    dob DATE DEFAULT NULL,
-    sexAtBirth CHAR(1) DEFAULT ''
-);
+ INSERT INTO People (firstName, lastName, address, email, password, phoneNumber, dob, gender, startDate, departmentPosition, radioNumber, stationNumber, isActive)
+ VALUES ('Kathryn', 'Pryde', '1123 Xavier School Drive
+ Watkinsville, GA 30677', 'kathryn@gmail.com', 'password1', '812-555-221', '04/02/2000', 'Female', '05/09/2018', 'Chief', 'A-1', '5', 'True');
 
-INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth) VALUES
-("SOME-REALLY-LONG-1234", "Sylvia", "Hernandez", "2012-09-01",  "F"),
-("SOME-REALLY-SHORT-5678", "Vish", "Balasubramanian", "1950-12-15",  "M"),
-("SOME-UNIQUE-ABCDE1", "J", "Doe", "1950-00-00",  ""),
-("SOME-DUMMY-DATA", "Pepper", "Potts", "1990-01-31",  "F");
+CREATE TABLE Certification (
+ certificationID int AUTO_INCREMENT,
+ agency varchar(255),
+ name varchar(255),
+ city varchar(255),
+ expirationPeriod varchar(255),
+ PRIMARY KEY (certificationID)
+ );
 
-CREATE TABLE PatientVisit (
-    visitId INTEGER PRIMARY KEY AUTO_INCREMENT,
-    patientGuid VARCHAR(64) UNIQUE,
-    visitDescription TEXT NOT NULL,
-    visitDateUtc DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    priority ENUM('low', 'medium', 'high') NOT NULL DEFAULT 'low'
+ INSERT INTO Certification (agency, name, city, expirationPeriod)
+ VALUES ('American Heart Association', 'Paramedic', 'Bloomington', 'July 2021');
 
-);
+CREATE TABLE certifiedPeople (
+ certifiedPeopleID int AUTO_INCREMENT,
+ userID int,
+ certificationID int,
+ certificationName varchar(255),
+ firstName varchar (255),
+ lastName varchar (255),
+ PRIMARY KEY (certifiedPeopleID),
+ FOREIGN KEY (userID) REFERENCES People(userID),
+ FOREIGN KEY (certificationID) REFERENCES Certification(certificationID)
+ );
 
-INSERT INTO PatientVisit (visitId, patientGuid, visitDescription) VALUES
-(1, 'SOME-REALLY-LONG-1234', 'Anxiety from D&S');
+ INSERT INTO certifiedPeople ( certificationName, firstName, lastName)
+ VALUES ('CPR', 'Joel', 'Embiid');
