1027960e998f39ad79680f1a654bc06e81c53ca3
Shirley Liu <liu471@umail.iu.edu>
2020-10-16 14:02:13 -0400

    Update index.app.js

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -1,95 +1,54 @@
 var app = new Vue({
   el: '#triagePage',
   data: {
-    certs:{
+    certs:[{
       agency:'',
       name:'',
       city:'',
       expirationPeriod:''
-    },
-
-
-    newCtForm: {}
-  },
-  computed: {
-
-  },
-  methods: {
-    createCerts(){
-      console.log("reached here");
-      // return {
-        //agency: "",
-        //name: "",
-        //city: "",
-        //expirationPeriod: ""
-       //};
-      console.log(this.certs.agency);
-      console.log(this.certs.name);
-      console.log(this.certs.city);
-      console.log(this.certs.expirationPeriod);
-      fetch('api/records/post.php', {
-        method:'POST',
-        body: JSON.stringify(this.newCtForm),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        }
-      })
-      .then( response => response.json() )
-      .then( json => {
-        console.log("Returned from post:", json);
-        // TODO: test a result was returned!
-        this.certs.push(json[0]);
-      });
-      console.log("Great Scott!!!")
-    },
-    //newPtData() {
-      //return {
-        //firstName: "",
-        //lastName: "",
-        //dob: "",
-        //sexAtBirth: ""
-      //}
-    //},
-    handleNewPtForm( evt ) {
-      // evt.preventDefault();  // Redundant w/ Vue's submit.prevent
-
-      // TODO: Validate the data!
-
-      fetch('api/records/post.php', {
-        method:'POST',
-        body: JSON.stringify(this.newCtForm),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        }
-      })
-      .then( response => response.json() )
-      .then( json => {
-        console.log("Returned from post:", json);
-        // TODO: test a result was returned!
-        this.certs.push(json[0]);
-      });
-
-      console.log("Creating (POSTing)...!");
-      console.log(this.newCtForm);
-
-      this.newCtForm = this.createCerts();
-    },
-    handleTriageForm( evt ) {
-      console.log("Form submitted!");
-
-      this.triageForm.pt = this.activePt;
-      console.log(this.triageForm);
-
+    }],
+    newCert: {
+      agency:'',
+      name:'',
+      city:'',
+      expirationPeriod:''
     }
   },
-  created() {
-    fetch("api/records/")
-    .then( response => response.json() )
-    .then( json => {
-      this.certs = json;
-
-      console.log(json)}
-    );
-    //this.newCtForm = this.newPtData();
-  }
-})
+  methods: {
+    fetchCerts(){
+      fetch('api/certification/')
+      .then( response => response.json() )
+      .then( json => {
+        this.certs = json;
+        console.log(this.certs)});
+      },
+     createCerts(){
+       fetch('api/certification/post.php', {
+         method:'POST',
+         body: JSON.stringify(this.newCert),
+         headers: {
+           "Content-Type": "application/json; charset=utf-8"
+         }
+       })
+       .then( response => response.json() )
+       .then( json => {
+         console.log("Returned from post:", json);
+         this.certs.push(json[0]);
+         this.newCert = this.newCertData();
+      });
+        console.log("Great Scott!!!");
+        console.log(this.newCert);
+     },
+     newCertData() {
+       return {
+         agency:'',
+         name:'',
+         city:'',
+         expirationPeriod:''
+       }
+     }
+},
+ created() {
+  this.fetchCerts();
+ }
+});
