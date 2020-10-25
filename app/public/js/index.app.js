bbcd2ea11d70fb39d8dd53e0924314bd3d7feb59
Shirley Liu <liu471@umail.iu.edu>
2020-10-25 18:07:40 -0400

    Update index.app.js
    updated deleteCerts()

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -2,19 +2,21 @@ var app = new Vue({
   el: '#triagePage',
   data: {
     certs:[{
+      certificationID:'',
       agency:'',
       name:'',
       city:'',
       expirationPeriod:''
     }],
     newCert: {
+      certificationID:'',
       agency:'',
       name:'',
       city:'',
       expirationPeriod:''
     }
   },
-  
+
   methods: {
     fetchCerts(){
       fetch('api/certification/index.php')
@@ -34,16 +36,17 @@ var app = new Vue({
        .then( response => response.json() )
        .then( json => {
          console.log("Returned from post:", json);
-         this.certs.push(json[0]);
+         this.certs = json;
          this.newCert = this.newCertData();
       });
         console.log("Great Scott!!!");
         console.log(this.newCert);
      },
-     deleteCerts(){
+     deleteCerts(){ var deleteCert;
+     console.log(deleteCert);
        fetch('api/certification/delete.php', {
          method:'POST',
-         body: JSON.stringify(this.newCert),
+         body: JSON.stringify(deleteCert),
          headers: {
            "Content-Type": "application/json; charset=utf-8"
          }
@@ -51,14 +54,15 @@ var app = new Vue({
        .then( response => response.json() )
        .then( json => {
          console.log("Returned from post:", json);
-         this.certs.push(json[0]);
+         this.certs = json;
          this.newCert = this.newCertData();
       });
         console.log("Great Scott!!!");
-        console.log(this.newCert);
+        console.log(deleteCert);
      },
      newCertData() {
        return {
+         certificationID:'',
          agency:'',
          name:'',
          city:'',
